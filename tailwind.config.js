3cd6e7ec879517c4847244fd543e5eb8b3f3a0de
Saransh Balyan <saransh.balyan@m2pfintech.com>
2025-04-14 02:47:44 +0530

    boorder variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,9 +24,12 @@ export default {
         manrope: ['Manrope', ...fontFamily.sans],
       },
       borderRadius: {
+        xl: 'calc(var(--radius) + 4px)',
+        '2xl': 'calc(var(--radius) + 8px)',
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
+        xs: 'calc(var(--radius) - 6px)',
       },
       colors: {
         background: 'hsl(var(--background))',
