103d946aea66743e3f3e94f0e281b2ffd6f378bb
Saransh Balyan <saransh.balyan@m2pfintech.com>
2025-04-26 21:52:08 +0530

    font

--- src/context/font-context.tsx
+++ src/context/font-context.tsx
@@ -14,7 +14,7 @@ export const FontProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [font, _setFont] = useState<Font>(() => {
-    const savedFont = localStorage.getItem('font')
+    const savedFont = localStorage.getItem('theme-font')
     return fonts.includes(savedFont as Font) ? (savedFont as Font) : fonts[0]
   })
 
@@ -31,7 +31,6 @@ export const FontProvider: React.FC<{ children: React.ReactNode }> = ({
   }, [font])
 
   const setFont = (font: Font) => {
-    localStorage.setItem('font', font)
     _setFont(font)
   }
 
