304d875a813078a95601c7805256359b5306ff90
Saransh Balyan <saransh.balyan@m2pfintech.com>
2025-04-30 21:14:05 +0530

    search

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -15,7 +15,7 @@ export function Search({ className = '', placeholder = 'Search' }: Props) {
     <Button
       variant='outline'
       className={cn(
-        'bg-muted/25 text-muted-foreground hover:bg-muted/50 relative h-8 w-full flex-1 justify-start rounded-md text-sm font-normal shadow-none sm:pr-12 md:w-40 md:flex-none lg:w-56 xl:w-64',
+        'bg-muted/25 text-muted-foreground hover:bg-muted/50 relative h-8 w-full flex-1 justify-start rounded-md text-sm font-normal shadow-none sm:pr-12 w-8 sm:w-50',
         className
       )}
       onClick={() => setOpen(true)}
@@ -24,7 +24,7 @@ export function Search({ className = '', placeholder = 'Search' }: Props) {
         aria-hidden='true'
         className='absolute top-1/2 left-1.5 -translate-y-1/2'
       />
-      <span className='ml-3'>{placeholder}</span>
+      <span className='ml-3 hidden sm:flex'>{placeholder}</span>
       <kbd className='bg-muted pointer-events-none absolute top-[0.3rem] right-[0.3rem] hidden h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none sm:flex'>
         <span className='text-xs'>⌘</span>K
       </kbd>
